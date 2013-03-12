e356854c285d5976fe2e830b5becd2fc26d1e082
zhiyixu <xuzy88@gmail.com>
2013-03-11 21:16:07 -0700

    Init

---
+++ CSCI520_A2_New/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  CSCI520_A2_New
+//
+//  Created by zhiyi xu on 3/11/13.
+//  Copyright (c) 2013 zhiyi xu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
